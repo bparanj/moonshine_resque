f44a05d547c837e490ac96c0d73a1d14f57378b6
Will Farrington <will@railsmachine.com>
2011-03-25 11:24:33 -0400

    More documentation.

--- generators/moonshine_resque_god/moonshine_resque_god_generator.rb
+++ generators/moonshine_resque_god/moonshine_resque_god_generator.rb
@@ -6,11 +6,25 @@ class MoonshineResqueGodGenerator < Rails::Generator::Base
     
       intro = <<-INTRO
 
-
 - To monitor Resque with God, install moonshine_god.
 
   script/plugin install git://github.com/railsmachine/moonshine_god.git
 
+You'll also want to add the `:resque` role to all servers using it.
+For example:
+
+  # config/deploy/production.rb
+  server 'worker1.myapp.com', :app, :resque
+
+We also strongly recommend having Moonshine automatically restart Resque on
+deploys:
+
+  # config/deploy.rb
+  before 'god:restart', 'god:resque:stop'
+
+This is a great way to handle automatically reloading new app code into your
+workers on deploy, as well as increasing or decreasing the number of
+Resque workers.
 
 INTRO
       
--- lib/generators/moonshine_resque_god/moonshine_resque_god_generator.rb
+++ lib/generators/moonshine_resque_god/moonshine_resque_god_generator.rb
@@ -16,6 +16,22 @@ class MoonshineResqueGodGenerator < Rails::Generators::Base
 
   script/plugin install git://github.com/railsmachine/moonshine_god.git
 
+You'll also want to add the `:resque` role to all servers using it.
+For example:
+
+  # config/deploy/production.rb
+  server 'worker1.myapp.com', :app, :resque
+
+We also strongly recommend having Moonshine automatically restart Resque on
+deploys:
+
+  # config/deploy.rb
+  before 'god:restart', 'god:resque:stop'
+
+This is a great way to handle automatically reloading new app code into your
+workers on deploy, as well as increasing or decreasing the number of
+Resque workers.
+
 INTRO
       
       puts intro
